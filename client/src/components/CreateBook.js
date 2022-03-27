23de1ecbcccbf2960f83eaf47dce13b9576fa9fe
poyanpayandeh <poyanpayandeh@hotmail.com>
2022-03-28 00:13:09 +1300

    fixed type

--- client/src/components/CreateBook.js
+++ client/src/components/CreateBook.js
@@ -58,7 +58,7 @@ class CreateBook extends Component {
             <div className="col-md-8 m-auto">
               <br />
               <Link to="/" className="btn btn-outline-warning float-left">
-                Show BooK List
+                Show Book List
               </Link>
             </div>
             <div className="col-md-8 m-auto">
