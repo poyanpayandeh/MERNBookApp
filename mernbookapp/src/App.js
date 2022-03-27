f2a4266b67026dec110d8848e153c3bea3d8ad4f
poyanpayandeh <poyanpayandeh@hotmail.com>
2022-03-27 19:00:14 +1300

    added cors and set it up in app.js

--- mernbookapp/src/App.js
+++ mernbookapp/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-com";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import CreateBook from "./components/CreateBook";
