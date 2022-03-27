b39810b17f41bf98707d513ddae7d8b6241d8b8a
poyanpayandeh <poyanpayandeh@hotmail.com>
2022-03-28 00:18:36 +1300

    updated port

--- client/src/components/ShowBookList.js
+++ client/src/components/ShowBookList.js
@@ -14,7 +14,7 @@ class ShowBookList extends Component {
 
   componentDidMount() {
     axios
-      .get("http://localhost:8082/api/books")
+      .get("http://localhost:8000/api/books")
       .then((res) => {
         this.setState({
           books: res.data,
