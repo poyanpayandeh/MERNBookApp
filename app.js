7302b226e36b3b800fea86849a0c6c5c0bf61a2e
poyanpayandeh <poyanpayandeh@hotmail.com>
2022-03-27 22:38:58 +1300

    set up heroku

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// app.js
-
 const express = require("express");
 const connectDB = require("./config/db");
+require("dotenv").config();
 var cors = require("cors");
+const path = require("path");
 
 // routes
 const books = require("./routes/api/books");
@@ -23,6 +23,12 @@ app.get("/", (req, res) => res.send("Hello world!"));
 // use Routes
 app.use("/api/books", books);
 
+app.use(express.static(path.join(__dirname, "client", "build")));
+
 const port = process.env.PORT || 8082;
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
