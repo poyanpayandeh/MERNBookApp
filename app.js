c16abe1ef15af36b18c951ff6bf794e22532f93b
poyanpayandeh <poyanpayandeh@hotmail.com>
2022-03-27 23:58:43 +1300

    updated port

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use("/api/books", books);
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 8000;
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
