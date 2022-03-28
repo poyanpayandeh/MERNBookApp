c7fd56bb2e1f6e3020555fb85ca0808507bcd939
poyanpayandeh <poyanpayandeh@hotmail.com>
2022-03-29 05:07:33 +1300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const books = require("./routes/api/books");
 const app = express();
 
 // cors
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors());
 
 // Connect Database
 connectDB();
