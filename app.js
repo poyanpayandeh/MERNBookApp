95b7db74d4f99c6ca49f72f754800b5da5651fa3
poyanpayandeh <poyanpayandeh@hotmail.com>
2022-03-29 07:15:09 +1300

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const port = process.env.PORT || 8082;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build")); // serve the static react app
-  app.get(/^\/(?!api).*/, (req, res) => {
+  app.get(books, (req, res) => {
     // don't serve api routes to react app
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
