4a14b41195dbf71de9ae92851ccad7b15af3869c
poyanpayandeh <poyanpayandeh@hotmail.com>
2022-03-29 05:34:48 +1300

    Update app.js

--- app.js
+++ app.js
@@ -4,21 +4,6 @@ require("dotenv").config();
 var cors = require("cors");
 const path = require("path");
 
-//CORS middleware
-var corsMiddleware = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8082/api/books"); //replace localhost with actual host
-  res.header(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, PUT, PATCH, POST, DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, X-Requested-With, Authorization"
-  );
-
-  next();
-};
-
 // routes
 const books = require("./routes/api/books");
 
@@ -36,7 +21,10 @@ app.use(express.json({ extended: false }));
 // use Routes
 app.use("/api/books", books);
 
-app.use(corsMiddleware);
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+  next();
+});
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
