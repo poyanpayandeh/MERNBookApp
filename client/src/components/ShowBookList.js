0e340f3bdaf9a6833ce9d28f5920da084ab1b16f
poyanpayandeh <poyanpayandeh@hotmail.com>
2022-03-30 00:52:00 +1300

    r

--- client/src/components/ShowBookList.js
+++ client/src/components/ShowBookList.js
@@ -14,7 +14,7 @@ class ShowBookList extends Component {
 
   componentDidMount() {
     axios
-      .get("http://localhost:8082/api/books")
+      .get("routes/api/books")
       .then((res) => {
         this.setState({
           books: res.data,
--- config/db.js
+++ config/db.js
@@ -15,4 +15,18 @@ const connectDB = async () => {
   }
 };
 
+// for local build
+/*const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+    });
+
+    console.log("MongoDB is Connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};*/
+
 module.exports = connectDB;
