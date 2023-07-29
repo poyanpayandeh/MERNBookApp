0ffb085536aaa417cadb7aadaabea3c73131c433
poyanpayandeh <81269820+poyanpayandeh@users.noreply.github.com>
2023-07-29 17:05:31 +1200

    set up routing

--- client/mernbookapp/src/App.js
+++ client/mernbookapp/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import CreateBook from "./components/CreateBook";
+import ShowBookList from "./components/ShowBookList";
+import ShowBookDetails from "./components/ShowBookDetails";
+import UpdateBookInfo from "./components/UpdateBookInfo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<ShowBookList />} />
+          <Route path="/create-book" element={<CreateBook />} />
+          <Route path="/edit-book/:id" element={<UpdateBookInfo />} />
+          <Route path="/show-book/:id" element={<ShowBookDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
