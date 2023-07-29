de5dae7d323132dcfd7404bf0a7f0e7b77c8d823
poyanpayandeh <81269820+poyanpayandeh@users.noreply.github.com>
2023-07-29 18:04:18 +1200

    installed cors and updated app.js

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
+
+//routes
+const books = require("./routes/api/books");
 
 const app = express();
 
 //connect Database
 connectDB();
 
+//cors
+app.use(cors({ origin: true, credentials: true }));
+
+//init middleware:
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("Hello World"));
+
+//use routes
+app.use("/api/books", books);
 
 const port = process.env.PORT || 8082;
 
